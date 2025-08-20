c83a912a9eb84ba05cb20d1c0f743c6c035ca6d5
Stefan Geranski <foxgamingbg12@gmail.com>
2025-08-20 13:04:08 +0300

    Add project files.

---
+++ Services/ConnectionManager.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+
+namespace DentisAPI.Services
+{
+    public static class ConnectionManager
+    {
+        public static List<MyConnection> cnnList = new List<MyConnection>();
+        private static IConfiguration? _Configuration;
+        public static int CommandTimeout { get; set; }
+        private static string? ConnectionString { get; set; }
+
+        public static IConfiguration? Configuration
+        {
+            get => _Configuration;
+            set
+            {
+                _Configuration = value;
+                if (value != null)
+                {
+                    CommandTimeout = value.GetValue<int>("CommandTimeout:Default");
+                    ConnectionString = value.GetConnectionString("DataConnection");
+                }
+            }
+        }
+
+        public static MyConnection? GetConnection(string User)
+        {
+            try
+            {
+                MyConnection? cnn;
+                lock (cnnList)
+                {
+                    cnn = cnnList.Find(x => x.InUse == false);
+                    if (cnn == null)
+                    {
+                        cnn = new MyConnection(ConnectionString, CommandTimeout);
+                        cnnList.Add(cnn);
+                    }
+                    else
+                    {
+                        cnn.InUse = true;
+                    }
+                    cnn.user = User;
+                }
+                return cnn!;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+    public class MyConnection
+    {
+        public SqlConnection cnn { get; set; }
+        public string? user { get; set; }
+        public bool InUse { get; set; }
+        public DateTime LastUsed { get; set; }
+        private SqlCommand _cmdExecuteAs { get; set; }
+        public MyConnection(string? ConnectionString, int CommandTimeout)
+        {
+            cnn = new SqlConnection(ConnectionString);
+            cnn.StateChange += new StateChangeEventHandler(cnn_StateChange);
+            InUse = true;
+            _cmdExecuteAs = new SqlCommand();
+            _cmdExecuteAs.Connection = cnn;
+            _cmdExecuteAs.CommandTimeout = CommandTimeout;
+            _cmdExecuteAs.CommandText = "EXECUTE AS USER = @username;";
+            _cmdExecuteAs.CommandType = CommandType.Text;
+            _cmdExecuteAs.Parameters.Add("@username", SqlDbType.NVarChar, 50);
+        }
+
+        private void cnn_StateChange(object sender, StateChangeEventArgs e)
+        {
+            if (e.CurrentState == ConnectionState.Open)
+            {
+                try
+                {
+                    _cmdExecuteAs.Parameters[0].Value = user;
+                    _cmdExecuteAs.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public async void Release()
+        {
+            try
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    await cnn.CloseAsync();
+                }
+            }
+            finally
+            {
+                InUse = false;
+            }
+        }
+    }
+}
+
---
+++ Services/EmailSender.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DentisAPI.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public IConfiguration Configuration { get; }
+        public EmailSender(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            SmtpClient client = new SmtpClient()
+            {
+                Port = int.Parse(Configuration["NetMail:port"] ?? throw new InvalidOperationException("NetMail:port missing")),
+                Host = Configuration["NetMail:smtpHost"] ?? throw new InvalidOperationException("NetMail:smtpHost missing"), //or another email sender provider
+                EnableSsl = bool.Parse(Configuration["NetMail:EnableSsl"] ?? throw new InvalidOperationException("NetMail:EnableSsl missing")),
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(Configuration["NetMail:sender"], Configuration["NetMail:senderpassword"])
+            };
+            MailMessage mm = new MailMessage(Configuration["NetMail:sender"] ?? throw new InvalidOperationException("NetMail missing"), email, subject, htmlMessage)
+            {
+                IsBodyHtml = true
+            };
+           return client.SendMailAsync(mm);
+        }
+    }
+}
